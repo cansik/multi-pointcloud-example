53d09f79452f05ed07a9a777ab9554b01a384273
Florian Bruggisser <florian@bildspur.ch>
2018-11-05 11:39:54 +0100

    added pcx with forest pointcloud

---
+++ Assets/Pcx/Editor/PlyImporter.cs
@@ -0,0 +1,306 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEditor;
+using UnityEditor.Experimental.AssetImporters;
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Pcx
+{
+    [ScriptedImporter(1, "ply")]
+    class PlyImporter : ScriptedImporter
+    {
+        #region ScriptedImporter implementation
+
+        public enum ContainerType { Mesh, ComputeBuffer  }
+
+        [SerializeField] ContainerType _containerType;
+
+        public override void OnImportAsset(AssetImportContext context)
+        {
+            if (_containerType == ContainerType.Mesh)
+            {
+                // Mesh container: Create a prefab with MeshFilter/MeshRenderer.
+                var gameObject = new GameObject();
+                var mesh = ImportAsMesh(context.assetPath);
+
+                var meshFilter = gameObject.AddComponent<MeshFilter>();
+                meshFilter.sharedMesh = mesh;
+
+                var meshRenderer = gameObject.AddComponent<MeshRenderer>();
+                meshRenderer.sharedMaterial = GetDefaultMaterial();
+
+                context.AddObjectToAsset("prefab", gameObject);
+                if (mesh != null) context.AddObjectToAsset("mesh", mesh);
+
+                context.SetMainObject(gameObject);
+            }
+            else
+            {
+                // ComputeBuffer container: Simply save it as a ScriptableObject.
+                var buffer = ImportAsPointCloudBuffer(context.assetPath);
+                context.AddObjectToAsset("buffer", buffer);
+                context.SetMainObject(buffer);
+            }
+        }
+
+        #endregion
+
+        #region Internal utilities
+
+        static Material GetDefaultMaterial()
+        {
+            return AssetDatabase.GetBuiltinExtraResource<Material>(
+                "Default-Material.mat"
+            );
+        }
+
+        #endregion
+
+        #region Internal data structure
+
+        enum DataProperty {
+            Invalid,
+            X, Y, Z,
+            R, G, B, A,
+            Data8, Data16, Data32
+        }
+
+        static int GetPropertySize(DataProperty p)
+        {
+            switch (p)
+            {
+                case DataProperty.X: return 4;
+                case DataProperty.Y: return 4;
+                case DataProperty.Z: return 4;
+                case DataProperty.R: return 1;
+                case DataProperty.G: return 1;
+                case DataProperty.B: return 1;
+                case DataProperty.A: return 1;
+                case DataProperty.Data8: return 1;
+                case DataProperty.Data16: return 2;
+                case DataProperty.Data32: return 4;
+            }
+            return 0;
+        }
+
+        class DataHeader
+        {
+            public List<DataProperty> properties = new List<DataProperty>();
+            public int vertexCount = -1;
+        }
+
+        class DataBody
+        {
+            public List<Vector3> vertices;
+            public List<Color32> colors;
+
+            public DataBody(int vertexCount)
+            {
+                vertices = new List<Vector3>(vertexCount);
+                colors = new List<Color32>(vertexCount);
+            }
+
+            public void AddPoint(
+                float x, float y, float z,
+                byte r, byte g, byte b, byte a
+            )
+            {
+                vertices.Add(new Vector3(x, y, z));
+                colors.Add(new Color32(r, g, b, a));
+            }
+        }
+
+        #endregion
+
+        #region Reader implementation
+
+        Mesh ImportAsMesh(string path)
+        {
+            try
+            {
+                var stream = File.Open(path, FileMode.Open, FileAccess.Read, FileShare.Read);
+                var header = ReadDataHeader(new StreamReader(stream));
+                var body = ReadDataBody(header, new BinaryReader(stream));
+
+                var mesh = new Mesh();
+                mesh.name = Path.GetFileNameWithoutExtension(path);
+
+                mesh.indexFormat = header.vertexCount > 65535 ?
+                    IndexFormat.UInt32 : IndexFormat.UInt16;
+
+                mesh.SetVertices(body.vertices);
+                mesh.SetColors(body.colors);
+
+                mesh.SetIndices(
+                    Enumerable.Range(0, header.vertexCount).ToArray(),
+                    MeshTopology.Points, 0
+                );
+
+                mesh.UploadMeshData(true);
+                return mesh;
+            }
+            catch (Exception e)
+            {
+                Debug.LogError("Failed importing " + path + ". " + e.Message);
+                return null;
+            }
+        }
+
+        PointCloudBuffer ImportAsPointCloudBuffer(string path)
+        {
+            try
+            {
+                var stream = File.Open(path, FileMode.Open, FileAccess.Read, FileShare.Read);
+                var header = ReadDataHeader(new StreamReader(stream));
+                var body = ReadDataBody(header, new BinaryReader(stream));
+                var buffer = ScriptableObject.CreateInstance<PointCloudBuffer>();
+                buffer.Initialize(body.vertices, body.colors);
+                return buffer;
+            }
+            catch (Exception e)
+            {
+                Debug.LogError("Failed importing " + path + ". " + e.Message);
+                return null;
+            }
+        }
+
+        DataHeader ReadDataHeader(StreamReader reader)
+        {
+            var data = new DataHeader();
+            var readCount = 0;
+
+            // Magic number line ("ply")
+            var line = reader.ReadLine();
+            readCount += line.Length + 1;
+            if (line != "ply")
+                throw new ArgumentException("Magic number ('ply') mismatch.");
+
+            // Data format: check if it's binary/little endian.
+            line = reader.ReadLine();
+            readCount += line.Length + 1;
+            if (line != "format binary_little_endian 1.0")
+                throw new ArgumentException(
+                    "Invalid data format ('" + line + "'). " +
+                    "Should be binary/little endian.");
+
+            // Read header contents.
+            for (var skip = false;;)
+            {
+                // Read a line and split it with white space.
+                line = reader.ReadLine();
+                readCount += line.Length + 1;
+                if (line == "end_header") break;
+                var col = line.Split();
+
+                // Element declaration (unskippable)
+                if (col[0] == "element")
+                {
+                    if (col[1] == "vertex")
+                    {
+                        data.vertexCount = Convert.ToInt32(col[2]);
+                        skip = false;
+                    }
+                    else
+                    {
+                        // Don't read elements other than vertices.
+                        skip = true;
+                    }
+                }
+
+                if (skip) continue;
+
+                // Property declaration line
+                if (col[0] == "property")
+                {
+                    var prop = DataProperty.Invalid;
+
+                    // Parse the property name entry.
+                    switch (col[2])
+                    {
+                        case "x"    : prop = DataProperty.X; break;
+                        case "y"    : prop = DataProperty.Y; break;
+                        case "z"    : prop = DataProperty.Z; break;
+                        case "red"  : prop = DataProperty.R; break;
+                        case "green": prop = DataProperty.G; break;
+                        case "blue" : prop = DataProperty.B; break;
+                        case "alpha": prop = DataProperty.A; break;
+                    }
+
+                    // Check the property type.
+                    if (col[1] == "char" || col[1] == "uchar")
+                    {
+                        if (prop == DataProperty.Invalid)
+                            prop = DataProperty.Data8;
+                        else if (GetPropertySize(prop) != 1)
+                            throw new ArgumentException("Invalid property type ('" + line + "').");
+                    }
+                    else if (col[1] == "short" || col[1] == "ushort")
+                    {
+                        if (prop == DataProperty.Invalid)
+                            prop = DataProperty.Data16;
+                        else if (GetPropertySize(prop) != 2)
+                            throw new ArgumentException("Invalid property type ('" + line + "').");
+                    }
+                    else if (col[1] == "int" || col[1] == "uint" || col[1] == "float")
+                    {
+                        if (prop == DataProperty.Invalid)
+                            prop = DataProperty.Data32;
+                        else if (GetPropertySize(prop) != 4)
+                            throw new ArgumentException("Invalid property type ('" + line + "').");
+                    }
+                    else
+                    {
+                        throw new ArgumentException("Unsupported property type ('" + line + "').");
+                    }
+
+                    data.properties.Add(prop);
+                }
+            }
+
+            // Rewind the stream back to the exact position of the reader.
+            reader.BaseStream.Position = readCount;
+
+            return data;
+        }
+
+        DataBody ReadDataBody(DataHeader header, BinaryReader reader)
+        {
+            var data = new DataBody(header.vertexCount);
+
+            float x = 0, y = 0, z = 0;
+            Byte r = 255, g = 255, b = 255, a = 255;
+
+            for (var i = 0; i < header.vertexCount; i++)
+            {
+                foreach (var prop in header.properties)
+                {
+                    switch (prop)
+                    {
+                        case DataProperty.X: x = reader.ReadSingle(); break;
+                        case DataProperty.Y: y = reader.ReadSingle(); break;
+                        case DataProperty.Z: z = reader.ReadSingle(); break;
+
+                        case DataProperty.R: r = reader.ReadByte(); break;
+                        case DataProperty.G: g = reader.ReadByte(); break;
+                        case DataProperty.B: b = reader.ReadByte(); break;
+                        case DataProperty.A: a = reader.ReadByte(); break;
+
+                        case DataProperty.Data8: reader.ReadByte(); break;
+                        case DataProperty.Data16: reader.BaseStream.Position += 2; break;
+                        case DataProperty.Data32: reader.BaseStream.Position += 4; break;
+                    }
+                }
+
+                data.AddPoint(x, y, z, r, g, b, a);
+            }
+
+            return data;
+        }
+    }
+
+    #endregion
+}
---
+++ Assets/Pcx/PointCloudRenderer.cs
@@ -0,0 +1,182 @@
+using UnityEngine;
+
+namespace Pcx
+{
+    [ExecuteInEditMode]
+    public class PointCloudRenderer : MonoBehaviour
+    {
+        #region Editable attributes
+
+        [SerializeField] PointCloudBuffer _source;
+        [SerializeField] Color _pointColor = Color.white;
+        [SerializeField] Color _reColor = Color.white;
+        [SerializeField] float _pointSize = 0.05f;
+
+        [Range(0, 1)]
+        [SerializeField] private float change = 0.01f;
+
+        #endregion
+
+        #region Internal resources
+
+        [SerializeField] Shader _shader;
+        [SerializeField] ComputeShader _sorter;
+
+        #endregion
+
+        #region Private variables
+
+        ComputeBuffer _pointBuffer;
+
+        Material _material1;
+        Material _material2;
+        Material _material3;
+
+        ComputeBuffer _drawBuffer1;
+        ComputeBuffer _drawBuffer2;
+        ComputeBuffer _drawBuffer3;
+
+        ComputeBuffer _argsBuffer1;
+        ComputeBuffer _argsBuffer2;
+        ComputeBuffer _argsBuffer3;
+
+        #endregion
+
+        #region MonoBehaviour methods
+
+        void OnValidate()
+        {
+            _pointSize = Mathf.Max(0, _pointSize);
+        }
+
+        void OnDisable()
+        {
+            // Note: This should be done in OnDisable, not in OnDestroy.
+            if (_pointBuffer != null)
+            {
+                _pointBuffer.Release();
+
+                _drawBuffer1.Release();
+                _drawBuffer2.Release();
+                _drawBuffer3.Release();
+
+                _argsBuffer1.Release();
+                _argsBuffer2.Release();
+                _argsBuffer3.Release();
+
+                _pointBuffer = null;
+
+                _drawBuffer1 = null;
+                _drawBuffer2 = null;
+                _drawBuffer3 = null;
+
+                _argsBuffer1 = null;
+                _argsBuffer2 = null;
+                _argsBuffer3 = null;
+            }
+        }
+
+        void OnDestroy()
+        {
+            if (_material1 != null)
+            {
+                if (Application.isPlaying)
+                {
+                    Destroy(_material1);
+                    Destroy(_material2);
+                    Destroy(_material3);
+                }
+                else
+                {
+                    DestroyImmediate(_material1);
+                    DestroyImmediate(_material2);
+                    DestroyImmediate(_material3);
+                }
+            }
+        }
+
+        void Update()
+        {
+            // Lazy initialization
+            if (_material1 == null)
+            {
+                _material1 = new Material(_shader);
+                _material2 = new Material(_shader);
+                _material3 = new Material(_shader);
+
+                _material1.hideFlags = HideFlags.DontSave;
+                _material2.hideFlags = HideFlags.DontSave;
+                _material3.hideFlags = HideFlags.DontSave;
+            }
+
+            if (_pointBuffer == null && _source != null)
+            {
+                _pointBuffer = _source.CreateComputeBuffer();
+
+                _drawBuffer1 = new ComputeBuffer(_source.pointCount, 4 * 4, ComputeBufferType.Append);
+                _drawBuffer2 = new ComputeBuffer(_source.pointCount, 4 * 4, ComputeBufferType.Append);
+                _drawBuffer3 = new ComputeBuffer(_source.pointCount, 4 * 4, ComputeBufferType.Append);
+
+                _argsBuffer1 = new ComputeBuffer(1, 4 * 4, ComputeBufferType.IndirectArguments);
+                _argsBuffer2 = new ComputeBuffer(1, 4 * 4, ComputeBufferType.IndirectArguments);
+                _argsBuffer3 = new ComputeBuffer(1, 4 * 4, ComputeBufferType.IndirectArguments);
+
+                _argsBuffer1.SetData(new uint[] { 0, 1, 0, 0 });
+                _argsBuffer2.SetData(new uint[] { 3 * 4, 0, 0, 0 });
+                _argsBuffer3.SetData(new uint[] { 3 * 12, 0, 0, 0 });
+            }
+        }
+
+        void OnRenderObject()
+        {
+            if (_material1 == null || _pointBuffer == null) return;
+
+            var proj = GL.GetGPUProjectionMatrix(Camera.current.projectionMatrix, true);
+            var view = Camera.current.worldToCameraMatrix;
+
+            _sorter.SetFloat("ScreenHeight", Camera.current.pixelHeight);
+            _sorter.SetVector("Extent", new Vector2(Mathf.Abs(proj[0, 0]), Mathf.Abs(proj[1, 1])) * _pointSize);
+            _sorter.SetMatrix("Transform", proj * view * transform.localToWorldMatrix);
+            _sorter.SetVector("ReColor", _reColor);
+            _sorter.SetFloat("change", change);
+
+            var kernel = _sorter.FindKernel("Main");
+            _sorter.SetBuffer(kernel, "PointBuffer", _pointBuffer);
+            _sorter.SetBuffer(kernel, "DrawBuffer1", _drawBuffer1);
+            _sorter.SetBuffer(kernel, "DrawBuffer2", _drawBuffer2);
+            _sorter.SetBuffer(kernel, "DrawBuffer3", _drawBuffer3);
+
+            _drawBuffer1.SetCounterValue(0);
+            _drawBuffer2.SetCounterValue(0);
+            _drawBuffer3.SetCounterValue(0);
+
+            _sorter.Dispatch(kernel, _source.pointCount / 128, 1, 1);
+
+            ComputeBuffer.CopyCount(_drawBuffer1, _argsBuffer1, 0);
+            ComputeBuffer.CopyCount(_drawBuffer2, _argsBuffer2, 4);
+            ComputeBuffer.CopyCount(_drawBuffer3, _argsBuffer3, 4);
+
+            _material1.SetPass(0);
+            _material1.SetColor("_Tint", _pointColor);
+            _material1.SetMatrix("_Transform", transform.localToWorldMatrix);
+            _material1.SetBuffer("_PointBuffer", _drawBuffer1);
+            Graphics.DrawProceduralIndirect(MeshTopology.Points, _argsBuffer1, 0);
+
+            _material2.SetPass(1);
+            _material2.SetColor("_Tint", _pointColor);
+            _material2.SetFloat("_PointSize", _pointSize);
+            _material2.SetMatrix("_Transform", transform.localToWorldMatrix);
+            _material2.SetBuffer("_PointBuffer", _drawBuffer2);
+            Graphics.DrawProceduralIndirect(MeshTopology.Triangles, _argsBuffer2, 0);
+
+            _material3.SetPass(2);
+            _material3.SetColor("_Tint", _pointColor);
+            _material3.SetFloat("_PointSize", _pointSize);
+            _material3.SetMatrix("_Transform", transform.localToWorldMatrix);
+            _material3.SetBuffer("_PointBuffer", _drawBuffer3);
+            Graphics.DrawProceduralIndirect(MeshTopology.Triangles, _argsBuffer3, 0);
+        }
+
+        #endregion
+    }
+}
